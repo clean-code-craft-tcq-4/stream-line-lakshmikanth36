0cd5724fff30137fda795b8d2a582be4582654a1
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-05 13:43:43 +0530

    Update TestSender.c

--- TestSender.c
+++ TestSender.c
@@ -5,15 +5,15 @@
 int sensorData;
 int i,count=0;
 #define MAX_VALUE 5
-float Temperature[MAX_VALUE];
-float Soc[MAX_VALUE];
+int Temperature[MAX_VALUE];
+int Soc[MAX_VALUE];
 int Generate_SensorData()
 {
     for(i=0;i<MAX_VALUE;i++)
     {
     Temperature[i] = rand()%50;
     Soc[i] = rand()%50;
-    printf("sendorData_Temperature=%f sendorData_Soc=%f, \n",Temperature[i],Soc[i]);
+    printf("sendorData_Temperature=%d sendorData_Soc=%d, \n",Temperature[i],Soc[i]);
     }
     return 0;
 }
