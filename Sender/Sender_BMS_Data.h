cc8d901879d2d9839e98cb8fb633223ec44d65bf
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-07 23:09:17 +0530

    Update Sender_BMS_Data.h

--- Sender/Sender_BMS_Data.h
+++ Sender/Sender_BMS_Data.h
@@ -4,6 +4,8 @@
 #define TEMP_RANGE  50
 #define SOC_RANGE   50
 #define CHAR_MAX    256
+#define DATA_OK     0 
+#define DATA_NOT_OK 1
 typedef struct {
 	int Temperature[MAX_VALUE];
 	int Soc[MAX_VALUE];
