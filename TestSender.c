bc25cb32d6dec4cfabfac4ddf1c820ec49f3f4a5
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-07 23:11:04 +0530

    Update TestSender.c

--- TestSender.c
+++ TestSender.c
@@ -4,7 +4,8 @@ int main()
 {
     sensorValue sensorData[MAX_VALUE];
     assert(GenerateSensorData(MAX_VALUE,sensorData)==0);
-    assert(!GenerateSensorData(MAX_VALUE,NULL));
-    assert(!GenerateSensorData(-1,NULL));
+    assert(GenerateSensorData(MAX_VALUE,NULL)==1);
+    assert(GenerateSensorData(-1,NULL)==1);
+    assert(GenerateSensorData(0,NULL)==1);
     return 0;
 }
