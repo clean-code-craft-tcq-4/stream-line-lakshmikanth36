4a200bcf78315dc29ba70ff547f4dec2c53db8cc
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-07 22:24:10 +0530

    Update Sender_BMS_Data.c

--- Sender/Sender_BMS_Data.c
+++ Sender/Sender_BMS_Data.c
@@ -8,16 +8,15 @@ void DisplayToConsole(char* buffer)
 }
 int GenerateSensorData(int Datasize,sensorValue *sensorData)
 {
-    	char buffer[CHAR_MAX] = {0};
-    	if(Datasize && (sensorData!=NULL))
-    	{
-        	for(int i=0;i<Datasize;i++)
-         	{
-           	sensorData->Temperature[i] = rand()%TEMP_RANGE;
-        	sensorData->Soc[i] = rand()%SOC_RANGE;
-        	sprintf(buffer,"%d,%d\n",sensorData->Temperature[i],sensorData->Soc[i]);
-        	DisplayToConsole(buffer);
-        	}
-        }	    
-    return 0;
-}
+    char buffer[CHAR_MAX] = {0};
+    if(Datasize && (sensorData!=NULL))
+    {
+        for(int i=0;i<Datasize;i++)
+        {
+        sensorData->Temperature[i] = rand()%TEMP_RANGE;
+        sensorData->Soc[i] = rand()%SOC_RANGE;
+        sprintf(buffer,"%d,%d\n",sensorData->Temperature[i],sensorData->Soc[i]);
+        DisplayToConsole(buffer);
+        }
+     }
+ }
