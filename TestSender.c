20b7db55f3ccd112f890bfb562d633832230925e
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-09-07 21:54:32 +0530

    Update TestSender.c
    Added code changes

--- TestSender.c
+++ TestSender.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-int sensorData;
-int i,count=0;
-#define MAX_VALUE 5
-int Temperature[MAX_VALUE];
-int Soc[MAX_VALUE];
-int Generate_SensorData()
+#define MAX_VALUE   50
+#define TEMP_RANGE  50
+#define SOC_RANGE   50
+#define CHAR_MAX    256
+#define DATA_OK     0 
+#define DATA_NOT_OK 1
+#define NULL        0
+
+typedef struct {
+	int Temperature[MAX_VALUE];
+	int Soc[MAX_VALUE];
+} sensorValue;
+
+void DisplayToConsole(char* buffer)
 {
-    for(i=0;i<MAX_VALUE;i++)
-    {
-    Temperature[i] = rand()%50;
-    Soc[i] = rand()%50;
-    printf("sendorData_Temperature=%d sendorData_Soc=%d\n",Temperature[i],Soc[i]);
-    }
-    return 0;
+	printf("%s",buffer);
 }
+
+int GenerateSensorData(int Datasize,sensorValue *sensorData)
+{
+    char buffer[CHAR_MAX] = {NULL};
+    if(Datasize && (sensorData!=NULL))
+    {
+        for(int i=0;i<Datasize;i++)
+        {
+        sensorData->Temperature[i] = rand()%TEMP_RANGE;
+        sensorData->Soc[i] = rand()%SOC_RANGE;
+        sprintf(buffer,"%d,%d\n",sensorData->Temperature[i],sensorData->Soc[i]);
+        DisplayToConsole(buffer);
+        }
+        return DATA_OK;
+    }
+    else
+    {
+        return DATA_NOT_OK;
+    }
+}
+
 
 int main()
 {
-    assert(Generate_SensorData()==0);
+    sensorValue sensorData[MAX_VALUE];
+    GenerateSensorData(MAX_VALUE,sensorData);
     return 0;
 }
