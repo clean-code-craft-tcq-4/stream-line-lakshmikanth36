e5bf3d5f4bcda43e72d12d6238f9bf7dbf18223b
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-15 14:51:44 +0530

    Update TestSender.c

--- TestSender.c
+++ TestSender.c
@@ -3,9 +3,7 @@
 int main()
 {
     sensorValue sensorData[MAX_VALUE];
-    assert(GenerateSensorData(MAX_VALUE,sensorData)==0);
-    assert(GenerateSensorData(MAX_VALUE,NULL)==1);
-    assert(GenerateSensorData(-1,NULL)==1);
-    assert(GenerateSensorData(0,NULL)==1);
+    assert(GenerateSensorData(sensorData)==0);
+    assert(GenerateSensorData(NULL)==1);
     return 0;
 }
