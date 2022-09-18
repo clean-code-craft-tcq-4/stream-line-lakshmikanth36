167bf86733052bf3cb00e1d4468b001060ec5f36
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 08:17:29 +0530

    Update ReceiveData.h

--- Receiver/ReceiveData.h
+++ Receiver/ReceiveData.h
@@ -1,4 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Receiver(int file_directory[], char receive_BMSdata[], int dataLength);
+#define MAX_VALUE   50
+
+#define MAXSENSORCNT   2
+#define LENGTH_OF_BMSDATA         8
+#define MAX_NOOF_RECEIVED_BMSDATA  (MAXSENSORCNT * MAX_VALUE * LENGTH_OF_BMSDATA)
+
+void Receiver(int file_directory[]);
