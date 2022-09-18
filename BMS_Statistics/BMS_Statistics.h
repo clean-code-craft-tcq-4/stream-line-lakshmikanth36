cb1e686c4b24c5c4cc213cae2830cd310bd92351
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 11:42:23 +0530

    Update BMS_Statistics.h

--- BMS_Statistics/BMS_Statistics.h
+++ BMS_Statistics/BMS_Statistics.h
@@ -8,3 +8,6 @@ void movingAverage_of_BMSdata(int BatteryTempdata[], int BatterySOCdata[]);
 void sortBmsData(int BatteryTempdata[], int BatterySOCdata[]);
 void sortGivenIndexPosition(int *BatteryTempdata, int *BatterySOCdata, int noOfElements, int received_position);
 void convertString_To_Int(char receive_BMSdata[], int len, int BatteryTempdata[], int BatterySOCdata[]);
+void convertBatteryTempData_To_Int(int receive_BMSdata, int BatteryTempdata[], char BMSdata[], int BMSData_length);
+void convertBatterySocData_To_Int(int receive_BMSdata, int BatterySOCdata[], char BMSdata[], int BMSData_length);
+
