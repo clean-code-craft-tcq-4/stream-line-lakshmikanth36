0ffc1ea72edf6b82ceffb4a783f701456664e286
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 10:51:12 +0530

    Create BMS_Statistics.h

---
+++ BMS_Statistics/BMS_Statistics.h
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#define   MAX_RECEIVE_DATA   50
+
+void BMSData_Statistics(char receive_BMSdata[], int datasize);
+void movingAverage_of_BMSdata(int BatteryTempdata[], int BatterySOCdata[]);
+void sortBmsData(int BatteryTempdata[], int BatterySOCdata[]);
+void sortGivenIndexPosition(int *BatteryTempdata, int *BatterySOCdata, int noOfElements, int received_position);
+void convertString_To_Int(char receive_BMSdata[], int len, int BatteryTempdata[], int BatterySOCdata[]);
