b16b2f271cd6273a947c680ff6bdb3df66d893db
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 17:11:59 +0530

    Update Test_Sender_Receiver_BMS_Statistics.c

--- Test_Sender_Receiver_BMS_Statistics.c
+++ Test_Sender_Receiver_BMS_Statistics.c
@@ -3,6 +3,16 @@
 #include "Receiver/Rx_BMS_Data.h"
 #include "BMS_Statistics/BMS_Statistics.h"
 
+int Rx_status = FALSE;
+char receive_BMSdata[MAX_NOOF_RECEIVED_BMSDATA];
+
+void Test_Receiver()
+{
+  Rx_status = ReadData_From_Console(READ_FROM_MOCK_DATA,receive_BMSdata);
+  assert(Rx_status == ACK_RX_DATA);
+}
+
 int main()
 {
+  Test_Receiver();
 }
