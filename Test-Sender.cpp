4a71a4b15c825bf826ded145aafdff5c6aff5b6d
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 17:01:20 +0530

    Delete Test-Sender.cpp

--- Test-Sender.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "catch.hpp"
-
-#include <stdlib.h>
-
-TEST_CASE("reports average, minimum and maximum") {
-}
