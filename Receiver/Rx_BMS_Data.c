c685b95c76ab72d15be8f2127c1be7f110d3b6c7
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 16:54:10 +0530

    Update Rx_BMS_Data.c

--- Receiver/Rx_BMS_Data.c
+++ Receiver/Rx_BMS_Data.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include "Rx_BMS_Data.h"
 #include "../BMS_Statistics/BMS_Statistics.h"
+#include "../Sender/Generate_BMS_Data.h"
 
 char receive_BMSdata[MAX_NOOF_RECEIVED_BMSDATA];
 char mock_BMSdata[MAX_NOOF_RECEIVED_BMSDATA] = {"33,36\n27,15\n43,35\n36,42\n49,21\n12,27\n40,9\n13,26\n40,26\n22,36\n11,18\n17,29\n32,30\n12,23\n17,35\n29,2\n22,8\n19,17\n43,6\n11,42\n29,23\n21,19\n34,37\n48,24\n15,20\n13,26\n41,30\n6,23\n12,20\n46,31\n5,25\n34,27\n36,5\n46,29\n13,7\n24,45\n32,45\n14,17\n34,14\n43,0\n37,8\n26,28\n38,34\n3,1\n4,49\n32,10\n26,18\n39,12\n26,36\n44,39\n"};
@@ -28,7 +29,7 @@ int ReadData_From_Console(receiveData rx_type, char ReadBMS_data[])
 {
 	int Tx_Status = Get_TransmitStatus();
 	
-	if((rx_type == READ_FROM_PIPE) && (Tx_Status == TRUE))
+	if((rx_type == READ_FROM_PIPE) && (Tx_Status == ACK_RX_DATA))
 	{
 		Read_from_PIPE(ReadBMS_data);
 		BMSData_Statistics(ReadBMS_data, MAX_NOOF_RECEIVED_BMSDATA);
