3bd4630b97e60d51e72dcfaa6cdecf046ee7a6f2
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 14:10:28 +0530

    Update Rx_BMS_Data.h

--- Receiver/Rx_BMS_Data.h
+++ Receiver/Rx_BMS_Data.h
@@ -7,4 +7,13 @@
 #define LENGTH_OF_BMSDATA         8
 #define MAX_NOOF_RECEIVED_BMSDATA  (MAXSENSORCNT * MAX_VALUE * LENGTH_OF_BMSDATA)
 
+typedef enum
+{
+    READ_FROM_PIPE = 0,
+    READ_FROM_MOCK_DATA
+}receiveData;
+
 void Receiver(int file_directory[]);
+void ReadData_From_Console(receiveData rx_type);
+void Read_from_PIPE(char ReadBMS_data[]);
+void Read_mock_data(char ReadBMS_data[]);
