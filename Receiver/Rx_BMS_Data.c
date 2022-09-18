df00256c2fdc1e141d4cb9e67282cc1677e33035
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 10:31:24 +0530

    Create Rx_BMS_Data.c

---
+++ Receiver/Rx_BMS_Data.c
@@ -0,0 +1,16 @@
+#include "Rx_BMS_Data.h"
+#include "Sender/Generate_BMS_Data.h"
+
+char receive_BMSdata[MAX_NOOF_RECEIVED_BMSDATA];
+
+void Receiver(int file_directory[])
+{
+	memset(receive_BMSdata, '\0', MAX_NOOF_RECEIVED_BMSDATA);
+		
+	close(file_directory[1]);
+		
+	read(file_directory[0], receive_BMSdata, MAX_NOOF_RECEIVED_BMSDATA);
+		
+	close(file_directory[0]);
+	//printf("Receiver: %s",receive_BMSdata);
+}
