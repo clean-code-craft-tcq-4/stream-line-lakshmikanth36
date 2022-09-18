420c2f25e14648d0c38d1ba75029fbe0139974a3
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-09-18 17:03:39 +0530

    Update and rename test_tx_rx.c to Test_Sender_Receiver_BMS_Statistics.c

---
+++ Test_Sender_Receiver_BMS_Statistics.c
@@ -0,0 +1,8 @@
+#include <assert.h>
+#include "Sender/Generate_BMS_Data.h"
+#include "Receiver/Rx_BMS_Data.h"
+#include "BMS_Statistics/BMS_Statistics.h"
+
+int main()
+{
+}
--- test_tx_rx.c
+++
@@ -1,6 +0,0 @@
-#include <assert.h>
-#include "Sender/Generate_BMS_Data.h"
-
-int main()
-{
-}
