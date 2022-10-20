01f40242bc617887b40ac6bd3e65f021a50b68fa
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-10-20 11:41:46 +0530

    Update receiver-test.cpp

--- Receiver/receiver-test.cpp
+++ Receiver/receiver-test.cpp
@@ -7,14 +7,19 @@
 #include <math.h>
 
 int Rx_status = FALSE;
-char Rx_BMSdata[MAX_NOOF_RECEIVED_BMSDATA];
+char Rx_BMSdata[MAX_NOOF_RECEIVED_BMSDATA] = {"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"};
 
 TEST_CASE("Read mock BMS data") {
     Rx_status = ReadData_From_Console(READ_FROM_MOCK_DATA,Rx_BMSdata);
-    REQUIRE(Rx_status == ACK_RX_DATA);
+    
+    for(int BMSdata = 0; BMSdata < 200; BMSdata++)
+    {
+        REQUIRE(Rx_BMSdata[BMSdata] != '\0');
+        REQUIRE(Rx_status == ACK_RX_DATA);
+    }
 }
 
-TEST_CASE("Read real time data: Test fails because Tx is not processed") {
+TEST_CASE("Read real time data from PIPE: Test fails because Tx is not processed") {
     Rx_status = ReadData_From_Console(READ_FROM_PIPE,Rx_BMSdata);
     REQUIRE(Rx_status == NACK_RX_DATA);
 }
