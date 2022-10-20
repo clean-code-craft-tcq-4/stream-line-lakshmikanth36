ac1ef27ced0047a33ddfb6f893c3e2636cb01beb
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-10-20 14:34:31 +0530

    Create BMSdata_Tx-test.cpp

---
+++ Sender/BMSdata_Tx-test.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "Generate_BMS_Data.h"
+
+#include <stdlib.h>
+#include <math.h>
+
+
+TEST_CASE("test") {
+    
+    //REQUIRE(Rx_status == ACK_RX_DATA);
+}
