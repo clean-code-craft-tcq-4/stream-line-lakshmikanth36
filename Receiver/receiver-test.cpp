1179c4b1c1003ee22ea84e6e9409b41ecc040162
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-10-20 10:59:43 +0530

    Update receiver-test.cpp

--- Receiver/receiver-test.cpp
+++ Receiver/receiver-test.cpp
@@ -13,3 +13,8 @@ TEST_CASE("Read mock BMS data") {
     Rx_status = ReadData_From_Console(READ_FROM_MOCK_DATA,Rx_BMSdata);
     REQUIRE(Rx_status == ACK_RX_DATA);
 }
+
+TEST_CASE("Read real time data: Test fails because Tx is not processed") {
+    Rx_status = ReadData_From_Console(READ_FROM_PIPE,Rx_BMSdata);
+    REQUIRE(Rx_status == NACK_RX_DATA);
+}
