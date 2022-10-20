68f5a65367e0918e4e573d46e14f773d3b0701cd
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-10-20 11:52:15 +0530

    Create catch.cpp

---
+++ BMS_Statistics/catch.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "BMS_Statistics.h"
+
+#include <stdlib.h>
+#include <math.h>
+
+TEST_CASE("test") {
+}
